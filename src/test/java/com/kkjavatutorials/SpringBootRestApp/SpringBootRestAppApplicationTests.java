9dd87139babd86b46a423eced9e73dce39efe5ab
sdgcpsb <76002842+sdgcpsb@users.noreply.github.com>
2021-01-18 22:38:56 +0530

    Update SpringBootRestAppApplicationTests.java

--- src/test/java/com/kkjavatutorials/SpringBootRestApp/SpringBootRestAppApplicationTests.java
+++ src/test/java/com/kkjavatutorials/SpringBootRestApp/SpringBootRestAppApplicationTests.java
@@ -18,17 +18,16 @@ import org.junit.runner.RunWith;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
 
-@RunWith(SpringRunner.class)
-@SpringBootTest
+
 
 public class SampleApplicationTests {
 
-	@Test
+	
 	public void passingTest() {
 		
 	}
 	
-	@Test
+	
 	public void failingTest() {
 		//assertTrue(false); // Uncomment this for the build to fail.
 	}
