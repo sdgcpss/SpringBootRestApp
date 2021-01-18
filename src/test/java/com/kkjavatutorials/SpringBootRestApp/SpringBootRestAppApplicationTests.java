813e2af1c0583b31c0e63c6fea168bafa7071caf
sdgcpsb <76002842+sdgcpsb@users.noreply.github.com>
2021-01-18 22:24:14 +0530

    Update SpringBootRestAppApplicationTests.java

--- src/test/java/com/kkjavatutorials/SpringBootRestApp/SpringBootRestAppApplicationTests.java
+++ src/test/java/com/kkjavatutorials/SpringBootRestApp/SpringBootRestAppApplicationTests.java
@@ -1,6 +1,6 @@
-//package com.kkjavatutorials.SpringBootRestApp;
+package com.kkjavatutorials.SpringBootRestApp;
 
-import org.junit.jupiter.api.Test;
+//import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
